1304dbe316b419b2401643009b1a5059f847898d
sarkhan <boy-ram@mail.ru>
2022-04-19 06:17:13 +0300

    done first task

--- 15_errors/01/index.js
+++ 15_errors/01/index.js
@@ -1,21 +1,33 @@
 export function calculateDiscount(price, percent) {
+  if (typeof price !== 'number' || typeof percent !== 'number') {
+    throw TypeError('Аргументы должны быть числами');
+  }
   return (price / 100) * percent;
 }
 
 export function getMarketingPrice(product) {
   const productObject = JSON.parse(product);
-
-  return productObject.prices.marketingPrice;
+  try {
+    return productObject.prices.marketingPrice;
+  } catch(error) {
+    return null;
+  }
 }
 
 // Функция имитирует неудачный запрос за картинкой
 function fetchAvatarImage(userId) {
   return new Promise((resolve, reject) => {
-    reject(new Error(`Error while fetching image for user with id ${userId}`));
+    if(userId) {
+      resolve();
+    } else reject(new Error(`Error while fetching image for user with id ${userId}`));
   });
 }
 
 export async function getAvatarUrl(userId) {
   const image = await fetchAvatarImage(userId);
-  return image.url;
+  try{
+    return image.url;
+  } catch(error) {
+    return '/images/default.jpg';
+  }
 }
