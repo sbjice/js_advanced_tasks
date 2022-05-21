299a0e4726f3f43234324605b593102066776700
sarkhan <boy-ram@mail.ru>
2022-05-21 20:14:26 +0300

    configured webpack

---
+++ 19_build/webpack.config.js
@@ -0,0 +1,59 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ImageMinimizerPlugin = require("image-minimizer-webpack-plugin");
+
+
+module.exports = (env) => ({
+  entry: './src/index.js',
+  output: {
+    filename: 'main.[contenthash].js',
+    publicPath: '/'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              "@babel/preset-env"
+            ]
+          }
+        }
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+
+    ]
+  },
+  optimization: {
+    minimizer: [
+      '...',
+      env.prod ? new ImageMinimizerPlugin({
+        minimizer: {
+          implementation: ImageMinimizerPlugin.imageminMinify,
+          options: {
+            plugins: [
+              ["optipng", { optimizationLevel: 5 }],
+            ],
+          },
+        },
+      }) : '...',
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Форма оплаты'
+    })
+  ],
+  devServer: {
+    historyApiFallback: true,
+    hot: true,
+    client: {
+      overlay: false,
+    },
+  },
+})
